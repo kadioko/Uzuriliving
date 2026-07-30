884323187d828c56656f7d8f4174cc7812cb13d6
kadioko <87696613+kadioko@users.noreply.github.com>
2026-07-30 13:21:54 +0300

    Adopt major TypeScript 7 update

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    useTypeScriptCli: true,
+  },
   async headers() {
     const contentSecurityPolicy = [
       "default-src 'self'",
